83ea08e94dedef9f7516f49e73123acc5161fd32
Aleksandr Alksandrov <hazg@mail.ru>
2021-03-12 10:14:09 +0300

    package

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,49 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import autoPreprocess from 'svelte-preprocess'
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import bundleSize from 'rollup-plugin-bundle-size'
+import analyze from 'rollup-plugin-analyzer'
 
-const pkg = require('./package.json');
+import pkg from './package.json'
+
+const production = !process.env.ROLLUP_WATCH
+
+const { name } = pkg
 
 export default {
-    input: 'src/number.svelte',
-    output: [
-        { file: pkg.module, 'format': 'en' },
-        { file: pkg.main, 'format': 'umd', name: 'Icon' }
-    ],
-    plugins: [
-        svelte(),
-        resolve()
-    ],
-};
+  input: 'src/number.svelte',
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: !production,
+      name
+    },
+    {
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: !production,
+      name
+    }
+  ],
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // generate: production ? 'dom' : 'ssr',
+      hydratable: true,
+      preprocess: autoPreprocess({
+        postcss: {
+          plugins: [require('autoprefixer')()]
+        }
+      })
+    }),
+    resolve(),
+    commonjs(),
+    production && terser(),
+    production && analyze(),
+    production && bundleSize(),
+  ],
+}
